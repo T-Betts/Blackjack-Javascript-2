d809137c3f0d64f2af1a3d66de3bb390492e4339
Tom Betts <tom.betts@live.co.uk>
2018-07-23 15:36:36 +0100

    Update Deck properties so that this.cards contains 2-Ace of each suit.

--- spec/DeckSpec.js
+++ spec/DeckSpec.js
@@ -1,8 +1,29 @@
 describe('Deck', function(){
+  let deck;
+  let testDeck;
+
+  beforeEach(function(){
+    deck = new Deck
+    testDeck = []
+    suits = ['s', 'h', 'd', 'c'];
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A'];
+    for (var i = 0; i < suits.length; i++) {
+      for (var x = 0; x < ranks.length; x++) {
+        testDeck.push(ranks[x] + suits[i]);
+      };
+    };
+  });
+
   describe('New Deck', function(){
-    it('has a card array containing 52 elements', function(){
-      deck = new Deck
+    it('has card array containing 52 elements', function(){
       expect(deck.cards.length).toEqual(52)
+    });
+
+    it('contains 2-Ace of each suit', function(){
+      var containsAll = testDeck.every(function(val) {
+        return deck.cards.indexOf(val) !== -1;
+      });
+      expect(containsAll).toBe(true);
     });
   });
 });
--- src/Deck.js
+++ src/Deck.js
@@ -1,6 +1,10 @@
 function Deck(){
   this.cards = []
-  for (var i = 0; i < 52; i++) {
-    this.cards.push(i);
+  this.suits = ['s', 'h', 'd', 'c']
+  this.ranks = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A']
+  for (var i = 0; i < this.suits.length; i++) {
+    for (var x = 0; x < this.ranks.length; x++) {
+      this.cards.push(this.ranks[x] + this.suits[i])
+    }
   }
 }
