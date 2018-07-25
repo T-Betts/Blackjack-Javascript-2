eecf0378ba1e19842c82777c1e4ab554f4925861
Tom Betts <tom.betts@live.co.uk>
2018-07-25 12:16:57 +0100

    Update GameSpec to allow Game to be tested in isolation.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,26 +1,43 @@
 describe('Game', function(){
+  let singleDeckPackDouble;
+  let onePlayerGame;
+
+  beforeEach(function(){
+    var testPack = [];
+    var suits = ['s', 'h', 'd', 'c'];
+    var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A'];
+    for (var i = 0; i < suits.length; i++) {
+      for (var x = 0; x < ranks.length; x++) {
+        testPack.push(ranks[x] + suits[i]);
+      };
+    };
+    singleDeckPackDouble = jasmine.createSpyObj('singleDeckPackDouble',['shuffle', 'remainingCards']);
+    singleDeckPackDouble.shuffle.and.returnValue(testPack);
+    singleDeckPackDouble.remainingCards.and.returnValue(testPack);
+    onePlayerGame = new Game(1, 1, singleDeckPackDouble);
+  });
+
   describe('#deal', function(){
     it('deals two cards to every player and the dealer', function(){
-      game = new Game(1, 4);
-      game.deal();
+      fourPlayerGame = new Game(1, 4, singleDeckPackDouble);
+      fourPlayerGame.deal();
+      console.log(fourPlayerGame.hands[0])
       function hasTwoCards(hand) {
         return hand.length === 2;
       }
-      expect(game.hands.every(hasTwoCards)).toBe(true);
+      expect(fourPlayerGame.hands.every(hasTwoCards)).toBe(true);
     });
   });
 
   describe('handScore', function(){
     it('should return soft value if hand containing an ace has a hard value more than 21', function(){
-      game = new Game(1, 1);
-      game.hands[0].push('5s', 'Ks', 'Ac');
-      expect(game.handScore(game.hands[0])).toEqual(16);
+      onePlayerGame.hands[0].push('5s', 'Ks', 'Ac');
+      expect(onePlayerGame.handScore(onePlayerGame.hands[0])).toEqual(16);
     });
 
     it('should return hard value if hand containing an ace has a hard value less than or equal to 21', function(){
-      game = new Game(1, 1)
-      game.hands[0].push('Ks', 'Ac');
-      expect(game.handScore(game.hands[0])).toEqual(21)
+      onePlayerGame.hands[0].push('Ks', 'Ac');
+      expect(onePlayerGame.handScore(onePlayerGame.hands[0])).toEqual(21)
     });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -19,7 +19,7 @@ Game.prototype.deal = function () {
   this.pack.shuffle()
   for (var round = 1; round <= 2; round++) {
     for (var i = 0; i < this.hands.length; i++) {
-      this.hands[i].push(this.pack.cards.pop());
+      this.hands[i].push(this.pack.remainingCards().pop());
     }
   }
 };
