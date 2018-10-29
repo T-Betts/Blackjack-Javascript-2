056abef6a999d175b40ba6b866a1068008b93d05
Tom Betts <tom.betts@live.co.uk>
2018-10-29 19:15:33 +0000

    Update Pack.js to ES6 syntax.

--- src/Pack.js
+++ src/Pack.js
@@ -1,26 +1,27 @@
 function Pack(deckCount = 1){
-  this.deckCount = deckCount
-  this.cards = []
-  this.suits = ['s', 'h', 'd', 'c']
-  this.ranks = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A']
-  for (var counter = 0; counter < this.deckCount; counter++) {
-    for (var i = 0; i < this.suits.length; i++) {
-      for (var x = 0; x < this.ranks.length; x++) {
-        this.cards.push(this.ranks[x] + this.suits[i]);
-      }
-    }
+  this.deckCount = deckCount;
+  this.cards = [];
+  this.suits = ['s', 'h', 'd', 'c'];
+  this.ranks = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A'];
+  for (let counter = 0; counter < this.deckCount; counter++) {
+    this.suits.forEach(suit => {
+      this.ranks.forEach((rank) => {
+        this.cards.push(rank + suit);
+      })
+    });
   }
+  
 }
 
 Pack.prototype.remainingCards = function () {
-  return this.cards
+  return this.cards;
 };
 
 // Durstenfeld Shuffle
 Pack.prototype.shuffle = function () {
-  for (var i = this.cards.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = this.cards[i];
+  for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = this.cards[i];
       this.cards[i] = this.cards[j];
       this.cards[j] = temp;
   }
